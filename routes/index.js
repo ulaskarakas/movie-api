e54b3b53a2949da5a9f8559dc460b648fc61b501
Ulaş Karakaş <ulaskarakas95@gmail.com>
2020-01-21 13:16:06 +0300

    writing register endpoint for user and password encryption with bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcryptjs');
+
+//Models
+const User = require('../models/User');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next) => {
+
+  const { username, password } = req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      username,
+      password: hash
+    });
+    const promise = user.save();
+
+    promise.then((data) => {
+      res.json(data); //res.json({status: 1});
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+
 });
 
 module.exports = router;
